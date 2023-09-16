233556226dd649425f95a6812b22b34cc4ffc1c2
EtiamSani <etiam.sani@laposte.net>
2023-09-16 09:20:06 +0200

    Edit profile picture message

--- app/ModalPicture.tsx
+++ app/ModalPicture.tsx
@@ -2,22 +2,22 @@ import { Button } from '@/components/ui/button'
 import { DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { editEmployeePicture } from '@/utils'
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ModalPictureProps {
   employeeId: number;
 }
 
 const ModalPicture = ({employeeId, onUpdateProfilePicture}: ModalPictureProps) => {
+  const [uploadMessage, setUploadMessage] = useState(false);
     const handlePictureChange = () => {
         const fileInput = document.getElementById('picture') as HTMLInputElement;
         const file = fileInput.files?.[0];
         if (file) {
-          
-          // editEmployeePicture(file, employeeId);
           editEmployeePicture(file, employeeId).then((newProfilePictureUrl) => {
             console.log(newProfilePictureUrl)
             onUpdateProfilePicture(employeeId, newProfilePictureUrl);
+            setUploadMessage(true)
           });
         }
       };
@@ -25,6 +25,8 @@ const ModalPicture = ({employeeId, onUpdateProfilePicture}: ModalPictureProps) =
     <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Ajouter une photo</DialogTitle>
+          {uploadMessage && 
+      <div className='text-green-600 m-auto font-bold bg-green-300 p-2 rounded-md'> La photo à bien été mis à jour</div>}
         </DialogHeader>
         <div>
             <Input id="picture" type="file" />
