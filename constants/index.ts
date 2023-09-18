3844aa0931afc1206a6e92f1f5af47fd2a67c15a
EtiamSani <etiam.sani@laposte.net>
2023-09-18 14:35:29 +0200

    Fixing bugs

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
 
 export const selectItems = [
     { value: 'extravertie', label: 'Extravertie' },
-    { value: 'introverti', label: 'Introverti' },
-    { value: 'ambiverti', label: 'Ambiverti' },
+    { value: 'introvertie', label: 'Introvertie' },
+    { value: 'ambivertie', label: 'Ambivertie' },
   ];
