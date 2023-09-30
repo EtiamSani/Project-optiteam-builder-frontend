b5e98d411bac0427f47e9b51b2010f39e929d5c3
EtiamSani <etiam.sani@laposte.net>
2023-09-30 20:29:31 +0200

    Add login page

---
+++ app/(root)/(auth)/sign-in/page.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+const page = () => {
+  return (
+    <Tabs defaultValue="account" className="w-[400px] mx-auto mt-[150px]">
+    <TabsList className="grid w-full grid-cols-2">
+      <TabsTrigger value="account">Connexion</TabsTrigger>
+      <TabsTrigger value="password">Inscription</TabsTrigger>
+    </TabsList>
+    <TabsContent value="account">
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-2xl'>Te revoilà !</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="email">Email</Label>
+            <Input id="name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="pasword">Mot de passe</Label>
+            <Input id="username" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button variant="yellow">Connecter</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+    <TabsContent value="password">
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-2xl'>Inscrivez-vous</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <div className="space-y-1">
+            <Label htmlFor="current">Pseudo</Label>
+            <Input id="current" type="password" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="current">Nom de l'équipe</Label>
+            <Input id="current" type="password" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="new">Email</Label>
+            <Input id="new" type="password" />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="new">Mot de passe</Label>
+            <Input id="new" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button variant="yellow">S'inscrire</Button>
+        </CardFooter>
+      </Card>
+    </TabsContent>
+  </Tabs>
+  )
+}
+
+export default page
--- next.config.js
+++ next.config.js
@@ -7,4 +7,11 @@ module.exports = {
   env: {
     API_URL: "http://localhost:3001",
   },
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/sign-in" }, // Page de connexion en tant que page d'accueil
+      "/sign-in": { page: "/sign-in" }, // Assurez-vous d'avoir une entrée pour /loginAndSignUp
+      // ... Autres pages de votre application
+    };
+  },
 };
