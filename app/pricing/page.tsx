9dd91237f1e8026af0a9ef86d04cd7a163858404
EtiamSani <etiam.sani@laposte.net>
2023-12-01 15:19:59 +0100

    pricing plan page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>pricing</div>
+    <div className='mb-8 mt-24 text-center max-w-5xl'>
+      <div className='mx-auto mb-10 sm:max-w-lg'>
+          <h1 className='text-6xl font-bold sm:text-7xl'>
+            Offres
+          </h1>
+          <p className='mt-5 text-gray-600 sm:text-lg'>
+            Whether you&apos;re just trying out our service
+            or need more, we&apos;ve got you covered.
+          </p>
+        </div>
+    </div>
   )
 }
 
